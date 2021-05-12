dce6fb9fc2bd2990975245f70bd9d78f4be16787
Codeissimple <>
2021-05-12 00:38:42 +0000

    1

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ struct date {
   int month;
   int day;
 } bob;
-void print_date(date* pointer);
+
+date *new_date();
+void print_date_by_reference(date*);
+void print_date_by_value(date);
 
 
 int main() {
   date *bob;
+  date *newDate;
   date halloween = {2021,10,31};
 
   cout << "halloween is on " << halloween.month << "/" << halloween.day <<"/"<<halloween.year << endl;
@@ -20,12 +24,32 @@ int main() {
   cout << "halloween is on " << bob->month << "/" << bob->day <<"/"<<bob->year << endl;
 
 
-  print_date(bob);
+  print_date_by_reference(bob);
 
+  print_date_by_value(halloween);
+
+  newDate = new_date();
+  print_date_by_reference(newDate);
 
   return 0;
 }
 
-void print_date(date* pointer){
+void print_date_by_reference(date* pointer){
   cout << "halloween is on " << pointer->month << "/" << pointer->day << "/" <<pointer->year << endl; 
+}
+
+void print_date_by_value(date aDate){
+  cout << "halloween is on " << aDate.month << "/" << aDate.day << "/" <<aDate.year << endl; 
+}
+
+date *new_date(){
+  date *aNewDate;
+
+  aNewDate = new date;
+
+  aNewDate->year = 0;
+  aNewDate->month = 0;
+  aNewDate->day = 0;
+
+  return aNewDate;
 }
